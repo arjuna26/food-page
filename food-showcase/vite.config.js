bd0e9408aab90200c6c72b15e048dab71daea311
arjuna26 <aherbst@zagmail.gonzaga.edu>
2025-03-08 01:14:26 +0000

    serving from pi

--- food-showcase/vite.config.js
+++ food-showcase/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  server: {
+    host: '0.0.0.0',  // Listen on all network interfaces
+    port: 3000,        // You can change the port if needed
+  },
 });
