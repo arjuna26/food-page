aa8af5223e642281bff3006aef9fb93b746c51a5
arjuna26 <aherbst@zagmail.gonzaga.edu>
2025-03-09 08:24:57 +0000

    insta icon has to be moved into seperate file, initial landing coming along

--- food-showcase/vite.config.js
+++ food-showcase/vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   server: {
-    host: '0.0.0.0',  // Listen on all network interfaces
-    port: 3000,        // You can change the port if needed
+    host: '0.0.0.0',
+    port: 3000,
   },
 });
