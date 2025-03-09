1ea8a056c3d268c03d31ef554af077ae0b7abd53
arjuna26 <aherbst@zagmail.gonzaga.edu>
2025-03-09 09:04:35 +0000

    image overlay color block

--- food-showcase/src/components/Landing.vue
+++ food-showcase/src/components/Landing.vue
@@ -2,8 +2,10 @@
     <div class="relative w-full h-[110vh] text-white overflow-hidden bg-darker">
         <div class="absolute z-10 w-full mx-auto py-30 px-0 grid grid-cols-12 bg-darker">
             <div class="absolute opacity-50 right-24 flex flex-row gap-10 py-20">
-                <a href="https://instagram.com/your-account"><img src="../icons/instagram.svg" alt="ig" class="w-12"></a>
-                <a href="https://instagram.com/your-account"><img src="../icons/soundcloud.svg" alt="ig" class="w-12"></a>
+                <a href="https://instagram.com/your-account"><img src="../icons/instagram.svg" alt="ig"
+                        class="w-12"></a>
+                <a href="https://instagram.com/your-account"><img src="../icons/soundcloud.svg" alt="ig"
+                        class="w-12"></a>
             </div>
             <div class="col-span-4 flex flex-col text-left gap-10 ml-24">
                 <h1 class="font-primary font-bold text-6xl text-light">
@@ -14,6 +16,12 @@
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore
                     et dolore magna aliqua.
                 </p>
+            </div>
+            <div class="col-span-6 m-24 relative w-full h-full">
+                <h1 class="text-4xl z-10 font-primary font-bold absolute top-[90%] translate-x-[180%] text-light">HELLO WORLD</h1>
+                <img src="../icons/rectangle.svg" alt="" class="absolute min-w-screen opacity-50">
+                <img src="https://placehold.co/500x800" alt="Placeholder"
+                    class="absolute to transform   ">
             </div>
         </div>
     </div>
